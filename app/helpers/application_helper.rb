dde89280c4cee5871ab78c8de4cdbebb25a3a06a
Vedant <vshetty.vs@gmail.com>
2020-01-07 02:52:43 +0530

    Implemented dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+#print date-time in a readable way
+  def readable_time(time)
+    time.strftime("%b %d, %I:%M%P")
+  end
+#checks if the current user is the creater of a post
+  def author_of(record)
+    user_signed_in? && current_users.id == record.user_id
+  end
+
+  def admin?
+    user_signed_in? && current_user.admin?
+  end
 end
