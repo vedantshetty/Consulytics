1845b8423bfbe2174b5ed104c5eff8d617ca59f3
Vedant <vshetty.vs@gmail.com>
2020-01-09 09:23:14 +0530

    Added admin-specific view all upcoming meetings

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'home#index'
+  get 'active_sessions', to: "meetings#active_sessions"
+  resources :users , only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
